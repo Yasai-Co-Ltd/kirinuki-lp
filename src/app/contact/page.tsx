22c43c1f8b7d8d5c7c11d41fd4b462135ffca85d
ryosuke-kubota <kubota.development@gmail.com>
2025-07-14 18:18:53 +0900

    fix contact degign

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,12 +9,6 @@ export default function Contact() {
       <section>
         <form>
           <h2 className="c">お問い合わせ<span>Contact</span></h2>
-          <p>当ページと同じ３項目のお問い合わせフォーム（自動フォーム試用版）を簡単に使えるようにセットしています。<br />
-          <span className="color-check">※当ページ（contact.html）はフォームの見本ページです。実際の自動フォームには使いませんのでご注意下さい。</span></p>
-          <p><span className="color-check">自動フォームを使う場合（※編集に入る前にご確認下さい）</span><br />
-          あなたのメールアドレス設定と、簡単な編集だけで使えます。<a href="https://template-party.com/file/formgen_manual_set2.html" target="_blank" rel="noopener noreferrer">こちらのマニュアルをご覧下さい。</a></p>
-          <p><span className="color-check">自動フォームを使わない場合</span><br />
-          テンプレートに梱包されている「form.html」「confirm.html」「finish.html」の3枚のファイルを削除して下さい。</p>
 
           <table className="ta1">
             <tbody>
